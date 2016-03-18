f0361d50b8186e96102fecd8d32428b88f764fcb
Aymericdd <aymeric42680@gmail.com>
2016-03-18 10:38:03 +0100

    Provisioning api core flask

--- Vagrantfile
+++ Vagrantfile
@@ -25,6 +25,8 @@ Vagrant.configure(2) do |config|
   # config.vm.network "forwarded_port", guest: 80, host: 8080
   config.vm.network "forwarded_port", guest: 8080, host: 8080
 
+  config.vm.provision "file", source: ".ssh", destination: "~/"
+
   # Create a private network, which allows host-only access to the machine
   # using a specific IP.
   # config.vm.network "private_network", ip: "192.168.33.10"
--- init.sh
+++ init.sh
@@ -43,6 +43,27 @@ sudo chmod +x /usr/local/bin/docker-compose
 echo "ACTIVE AUTO COMPLETION DOCKER COMPOSE"
 sudo curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
 
+# Sources
+# Set ssh right
+echo "SET SSH RIGHT"
+sudo cp /home/vagrant/.ssh/* /root/.ssh/
+chmod 600 /root/.ssh/*
+sudo ssh-keyscan gitlab.aymericdaurelle.ovh > /root/.ssh/known_hosts
+
+# Set directory work
+echo "CREATE WORK DIRECTORIES"
+rm -R /docker
+
+echo "CLONE DOCKER IMAGES INFRA"
+git clone git@gitlab.aymericdaurelle.ovh:transcodesupinfo/infra-transcode.git /docker
+
+echo "CLONE DOCKER IMAGES CORE API"
+git clone -b basic-api-service git@gitlab.aymericdaurelle.ovh:transcodesupinfo/core-api.git /docker/core-api
+
+# Set permissions
+echo "SET RIGHT PERMISSIONS DIRECTORY"
+chown -R vagrant:vagrant /docker
+
 # Restart server
 echo "RESTART SERVER"
 sudo init 6
