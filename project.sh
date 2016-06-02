475ff2941d7bd631603bbe5b80c3def9a32cc4ea
Aymericdd <aymeric42680@gmail.com>
2016-06-02 22:06:45 +0200

    Change structure and set permissions folders

--- project.sh
+++ project.sh
@@ -10,13 +10,19 @@ echo "SET RIGHT PERMISSIONS DIRECTORY"
 sudo chown -R vagrant:vagrant /docker
 
 echo "CLONE DOCKER IMAGES INFRA"
-git clone -b develop git@gitlab.aymericdaurelle.ovh:transcodesupinfo/infra-transcode.git /docker
+git clone -b develop git@gitlab.aymericdaurelle.ovh:transcodesupinfo/infra-transcode.git /docker/transcode
 
 echo "CLONE DOCKER IMAGES CORE API"
-git clone -b develop git@gitlab.aymericdaurelle.ovh:transcodesupinfo/core-api.git /docker/core/core-api
+git clone -b develop git@gitlab.aymericdaurelle.ovh:transcodesupinfo/core-api.git /docker/transcode/core/core-api
 
 echo "CLONE DOCKER IMAGES WEB APP"
-git clone -b develop git@gitlab.aymericdaurelle.ovh:transcodesupinfo/web-app.git /docker/web/app
+git clone -b develop git@gitlab.aymericdaurelle.ovh:transcodesupinfo/web-app.git /docker/transcode/web/app
+
+# Set rights
+echo "SET RIGHT PERMISSIONS"
+sudo chown -R vagrant:vagrant /docker
+sudo chmod -R 775 /docker
+
 
 # Restart server
 echo "RESTART SERVER"
